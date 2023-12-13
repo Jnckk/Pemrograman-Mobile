696bb4a7227af472522d9bf3a0864982add4263f
Jnckk <riskicahgendeng@gmail.com>
2023-12-13 18:55:05 +0700

    Latihan Modul 6

--- 6. testing/integration_test/app_test.dart
+++ 6. testing/integration_test/app_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:testing/materi/integration/todo_page.dart';
+// RUN => flutter test integration_test/app_test.dart
 
 void main() {
   IntegrationTestWidgetsFlutterBinding
